c72f12bf61ed17262e7c9ecf5e34934466b0b20b
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2024-05-16 14:58:06 +0300

    Auth - LogoutController - maked logout only current device

--- app/Http/Controllers/API/v1/Auth/LogoutController.php
+++ app/Http/Controllers/API/v1/Auth/LogoutController.php
@@ -5,15 +5,24 @@ namespace App\Http\Controllers\API\v1\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
 
 class LogoutController extends Controller
 {
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, bool $allDevices = false)
     {
-        Auth::logout();
+        if ($allDevices) {
+            // Выход со всех устройств, обновляем remember_token
+            Auth::logout();
+        } else {
+            // Выход только с текущего устройства, не обновляем remember_token
+            $request->session()->invalidate();
+            $request->session()->regenerateToken();
 
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
+            // Удаление remember_token на стороне клиента
+            $cookie = Cookie::forget(Auth::getRecallerName());
+            return response('Logged out')->withCookie($cookie);
+        }
 
         return response()->noContent();
     }
