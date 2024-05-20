d50d232f15f60f4053000e2b3c62221b058761c5
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2024-05-20 09:53:02 +0300

    Auth - LogoutController - Added comments

--- app/Http/Controllers/API/v1/Auth/LogoutController.php
+++ app/Http/Controllers/API/v1/Auth/LogoutController.php
@@ -12,10 +12,10 @@ class LogoutController extends Controller
     public function __invoke(Request $request, bool $allDevices = false)
     {
         if ($allDevices) {
-            // Выход со всех устройств, обновляем remember_token
+            // Выход со всех устройств (обновляем remember_token)
             Auth::logout();
         } else {
-            // Выход только с текущего устройства, не обновляем remember_token
+            // Выход только с текущего устройства (не обновляем remember_token)
             $request->session()->invalidate();
             $request->session()->regenerateToken();
 
