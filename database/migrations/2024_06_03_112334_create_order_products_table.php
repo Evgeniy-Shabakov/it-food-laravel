29f3ab5585de2585be7a335c35272cbea49a9c39
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2024-06-03 14:38:17 +0300

    Order_products - Added table

---
+++ database/migrations/2024_06_03_112334_create_order_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('product_id');
+            $table->integer('quantity');
+            $table->decimal('price', 10, 2);
+            $table->timestamps();
+
+            $table->index('order_id', 'order_products_order_idx');
+            $table->foreign('order_id', 'order_products_order_fk')->on('orders')->references('id');
+
+            $table->index('product_id', 'order_products_product_idx');
+            $table->foreign('product_id', 'order_products_product_fk')->on('products')->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_products');
+    }
+};
