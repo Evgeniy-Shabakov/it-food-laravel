c2b332a0f0e0e8924f62667fd7ad4682a0b68b38
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2024-07-17 03:19:11 +0300

    Order - OrderIndexTodayController - Added a time zone correction

--- app/Http/Controllers/API/v1/Order/OrderIndexTodayController.php
+++ app/Http/Controllers/API/v1/Order/OrderIndexTodayController.php
@@ -5,13 +5,19 @@ namespace App\Http\Controllers\API\v1\Order;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\API\v1\Order\OrderResource;
 use App\Models\Order;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class OrderIndexTodayController extends Controller
 {
-    public function __invoke()
+    public function __invoke(Request $request)
     {
-        $today = now()->startOfDay();
-        $orders = Order::whereDate('created_at', $today)->get();
+        $timezone = $request->query('timezone', 'UTC'); // Получаем часовой пояс из запроса, по умолчанию 'UTC'
+
+        $startOfDay = Carbon::now($timezone)->startOfDay()->setTimezone('UTC');
+        $endOfDay = Carbon::now($timezone)->endOfDay()->setTimezone('UTC');
+
+        $orders = Order::whereBetween('created_at', [$startOfDay, $endOfDay])->get();
 
         return OrderResource::collection($orders);
     }
