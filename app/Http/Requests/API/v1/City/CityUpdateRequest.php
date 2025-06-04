2f7f721858aa99dc98e57c12aba8db6ba9d2ae71
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2025-06-04 08:42:35 +0300

    City - Added map_iframe and geojson

--- app/Http/Requests/API/v1/City/CityUpdateRequest.php
+++ app/Http/Requests/API/v1/City/CityUpdateRequest.php
@@ -7,55 +7,60 @@ use Illuminate\Validation\Rule;
 
 class CityUpdateRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
+   /**
+    * Determine if the user is authorized to make this request.
+    */
+   public function authorize(): bool
+   {
+      return true;
+   }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
-     */
-    public function rules(): array
-    {
-        return [
-            'title' => [
-                'required',
-                'string',
-                Rule::unique('cities')->ignore($this->city)],
+   /**
+    * Get the validation rules that apply to the request.
+    *
+    * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+    */
+   public function rules(): array
+   {
+      return [
+         'title' => [
+            'required',
+            'string',
+            Rule::unique('cities')->ignore($this->city)
+         ],
 
-            'country_id' => [
-                'required',
-                'integer',
-                'exists:countries,id'],
-            'min_order_value_for_delivery' => 'numeric|min:0|max:999999',
-            'delivery_price' => 'numeric|min:0|max:999999',
-            'order_value_for_free_delivery' => 'numeric|min:0|max:999999',
-        ];
-    }
+         'country_id' => [
+            'required',
+            'integer',
+            'exists:countries,id'
+         ],
+         'geojson' => ['array'],
+         'min_order_value_for_delivery_by_default' => 'numeric|min:0|max:999999',
+         'delivery_price_by_default' => 'numeric|min:0|max:999999',
+         'order_value_for_free_delivery_by_default' => 'numeric|min:0|max:999999',
+         'map_iframe' => 'nullable|string',
+         'geojson' => 'nullable|json',
+      ];
+   }
 
-    public function messages(): array
-    {
-        return [
-            'title.required' => 'Поле "город" обязательно для заполнения',
-            'title.string' => 'Поле "город" должно быть строковым значением',
-            'title.unique' => 'Такой город уже есть в базе данных',
-            'country_id.required' => 'Поле "страна" обязательно для заполнения',
-            'country_id.integer' => 'Поле "страна" должно быть целым числом',
-            'country_id.exists' => 'Страна не добавлена в список стран',
-            'min_order_value_for_delivery.numeric' => 'Поле должно быть числом',
-            'min_order_value_for_delivery.min' => 'Поле должно быть больше или равно 0',
-            'min_order_value_for_delivery.max' => 'Поле должно быть меньше 999999',
-            'delivery_price.numeric' => 'Поле должно быть числом',
-            'delivery_price.min' => 'Поле должно быть больше или равно 0',
-            'delivery_price.max' => 'Поле должно быть меньше 999999',
-            'order_value_for_free_delivery.numeric' => 'Поле должно быть числом',
-            'order_value_for_free_delivery.min' => 'Поле должно быть больше или равно 0',
-            'order_value_for_free_delivery.max' => 'Поле должно быть меньше 999999',
-        ];
-    }
+   public function messages(): array
+   {
+      return [
+         'title.required' => 'Поле "город" обязательно для заполнения',
+         'title.string' => 'Поле "город" должно быть строковым значением',
+         'title.unique' => 'Такой город уже есть в базе данных',
+         'country_id.required' => 'Поле "страна" обязательно для заполнения',
+         'country_id.integer' => 'Поле "страна" должно быть целым числом',
+         'country_id.exists' => 'Страна не добавлена в список стран',
+         'min_order_value_for_delivery.numeric' => 'Поле должно быть числом',
+         'min_order_value_for_delivery.min' => 'Поле должно быть больше или равно 0',
+         'min_order_value_for_delivery.max' => 'Поле должно быть меньше 999999',
+         'delivery_price.numeric' => 'Поле должно быть числом',
+         'delivery_price.min' => 'Поле должно быть больше или равно 0',
+         'delivery_price.max' => 'Поле должно быть меньше 999999',
+         'order_value_for_free_delivery.numeric' => 'Поле должно быть числом',
+         'order_value_for_free_delivery.min' => 'Поле должно быть больше или равно 0',
+         'order_value_for_free_delivery.max' => 'Поле должно быть меньше 999999',
+      ];
+   }
 }
--- database/migrations/2023_09_02_093547_create_cities_table.php
+++ database/migrations/2023_09_02_093547_create_cities_table.php
@@ -6,30 +6,32 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('cities', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
-            $table->unsignedBigInteger('country_id');
-            $table->decimal('min_order_value_for_delivery', 8, 2)->default(0);
-            $table->decimal('delivery_price', 8, 2)->default(0);
-            $table->decimal('order_value_for_free_delivery', 8, 2)->default(0);
-            $table->timestamps();
+   /**
+    * Run the migrations.
+    */
+   public function up(): void
+   {
+      Schema::create('cities', function (Blueprint $table) {
+         $table->id();
+         $table->string('title');
+         $table->unsignedBigInteger('country_id');
+         $table->decimal('min_order_value_for_delivery_by_default', 8, 2)->default(0);
+         $table->decimal('delivery_price_by_default', 8, 2)->default(0);
+         $table->decimal('order_value_for_free_delivery_by_default', 8, 2)->nullable();
+         $table->text('map_iframe')->nullable();
+         $table->json('geojson')->nullable();
+         $table->timestamps();
 
-            $table->index('country_id', 'city_country_idx');
-            $table->foreign('country_id', 'city_country_fk')->on('countries')->references('id');
-        });
-    }
+         $table->index('country_id', 'city_country_idx');
+         $table->foreign('country_id', 'city_country_fk')->on('countries')->references('id');
+      });
+   }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('cities');
-    }
+   /**
+    * Reverse the migrations.
+    */
+   public function down(): void
+   {
+      Schema::dropIfExists('cities');
+   }
 };
